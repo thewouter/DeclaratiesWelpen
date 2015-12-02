9e30f12bfdbb035da169a0edb019a3f949352cb1
Wouter van Harten <woutervh@live.nl>
2015-12-02 23:52:57 +0100

    Fix for error when no excel file exists

--- overview.php
+++ overview.php
@@ -3,20 +3,20 @@
 <?php 
 $speltakken = array(0=>"jongens", 1=>"meisjes");
 foreach($speltakken as $speltak){
-	?>
-	<table >
-		<thead> 
-			<tr>
-				<th>Declaratie</th>
-				<th>Speltak</th>
-				<th id='name'>Naam</th>
-				<th>akkord</th>
-			</tr>
-		</thead>
-		<tbody>
+	$excel = getcwd() . "/" . $speltak . "/" . date("Y") . "/data.xlsx";
+	if(file_exists($excel)){
+		?>
+			<table >
+				<thead> 
+					<tr>
+						<th>Declaratie</th>
+						<th>Speltak</th>
+						<th id='name'>Naam</th>
+						<th>akkoord</th>
+					</tr>
+				</thead>
+			<tbody>
 		<?php
-		$excel = getcwd() . "/" . $speltak . "/" . date("Y") . "/data.xlsx";
-	
 		require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
 		$ea = PHPExcel_IOFactory::load($excel);
 		
@@ -31,14 +31,14 @@ foreach($speltakken as $speltak){
 					echo"<td>" . $declaration . "</td>";
 					echo"<td>" . $speltak . "</td>";
 					echo"<td id='name'>" . $name . "</td>";
-					echo"<td>d</td>";
+					echo"<td></td>";
 				echo"</tr>";
-		}
-		
-		?>
+		}?>
 		</tbody>
 	</table>
 	<br>
-<?php 
+	<?php 
+	}
+		
 }
 ?>
