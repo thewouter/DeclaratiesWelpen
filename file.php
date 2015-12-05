a21804126534128c9dcaa1657a9dc4e51fcefa4d
Wouter van Harten <woutervh@live.nl>
2015-12-05 12:10:07 +0100

    Style update to the form

--- file.php
+++ file.php
@@ -32,25 +32,26 @@
 </script>
 <h3>Declaratieformulier</h3>
 <p>Vul dit formulier in om je bonnetje in te leveren.</p>
-<br>
-<div class="form">	
-	<form action="file_upload_parser" method="post" enctype="multipart/form-data" id="file"> 
-		<p>Bonnetje: <input type="file" name="fileToUpload" id="fileToUpload" onchange="uploadFile()" required/></p>
-		<progress id="progressBar" value="0" max="100"></progress>
-	</form> 	
-</div>
-<br>
-<div class="form">
-	<form action="index.php?loc=action" method="post" enctype="multipart/form-data" id="data">
-		 <p> Speltak: <select name="speltak" required>
-			 <option value="jongens">Welpen Jongens </option>
-			 <option value="meisjes">Welpen Meisjes </option> </select></p>
-		 <p>Naam: <input type="text" name="name" required/></p>
-		 <p>IBAN: <input type="text" name="iban" required/></p>
-		 <p>Rek. Houder: <input type="text" name="rekhouder" required/></p>
-		 <p>Bedrag in &#8364: <input class="money" type="number" step="0.01" name="amount" required/></p>
-		<p>Omschrijving: <textarea rows="3" cols="20" name='comment' placeholder="omschrijving" required></textarea>
-		<p><input type="submit" value="Bonnetje indienen" name="submit" id="submit" disabled=true /></p>
-	</form>
-	</div>
 
+<div class="declarate_form">
+	<div class="form">	
+		<form action="file_upload_parser" method="post" enctype="multipart/form-data" id="file"> 
+			<p>Bonnetje: <input type="file" name="fileToUpload" id="fileToUpload" onchange="uploadFile()" required/></p>
+			<progress id="progressBar" value="0" max="100"></progress>
+		</form> 	
+	</div>
+	<br>
+	<div class="form">
+		<form action="index.php?loc=action" method="post" enctype="multipart/form-data" id="data">
+			 <p> Speltak: <select name="speltak" required>
+				 <option value="jongens">Welpen Jongens </option>
+				 <option value="meisjes">Welpen Meisjes </option> </select></p>
+			 <p>Naam: <input type="text" name="name" required/></p>
+			 <p>IBAN: <input type="text" name="iban" required/></p>
+			 <p>Rek. Houder: <input type="text" name="rekhouder" required/></p>
+			 <p>Bedrag in &#8364: <input class="money" type="number" step="0.01" name="amount" required/></p>
+			<p>Omschrijving: <textarea rows="3" cols="20" name='comment' placeholder="omschrijving" required></textarea>
+			<p><input type="submit" value="Bonnetje indienen" name="submit" id="submit" disabled=true /></p>
+		</form>
+	</div>
+</div>
